7579fcf5088279253ef596011393cb6c6e093876
masterzhk <masterzhk@163.com>
2021-05-17 14:35:41 +0800

    添加请求键选择不依赖请求结果的情况优化处理

--- Synchronizer/RequestSynchronizer.cs
+++ Synchronizer/RequestSynchronizer.cs
@@ -30,6 +30,11 @@ namespace Synchronizer
         private Func<TRequestInput, TRequestOutput, TKey> m_RequestKeySelector;
 
         /// <summary>
+        /// 请求键选择是否依赖请求结果
+        /// </summary>
+        private bool m_RequestKeySelectorWithRequestOutput;
+
+        /// <summary>
         /// 异步结果键选择器
         /// </summary>
         private Func<TResponse, TKey> m_ResponseKeySelector;
@@ -60,6 +65,32 @@ namespace Synchronizer
             m_RequestFunc = requestFunc;
             m_RequestFilter = requestFilter;
             m_RequestKeySelector = requestKeySelector;
+            m_RequestKeySelectorWithRequestOutput = true;
+            m_ResponseKeySelector = responseKeySelector;
+        }
+
+        /// <summary>
+        /// 创建一个同步器
+        /// </summary>
+        /// <param name="requestFunc">请求方法</param>
+        /// <param name="requestFilter">请求过滤器</param>
+        /// <param name="requestKeySelector">请求键选择器</param>
+        /// <param name="responseKeySelector">异步结果键选择器</param>
+        /// <remarks>
+        /// 请求过滤器requestFilter返回true表示是有效的请求，否则请求无效，不同步异步结果。
+        /// 请求键选择器返回的键与异步结果键选择器返回的键相等表示同步成功，否则同步失败。
+        /// </remarks>
+        public RequestSynchronizer(
+            Func<TRequestInput, TRequestOutput> requestFunc,
+            Func<TRequestInput, TRequestOutput, bool> requestFilter,
+            Func<TRequestInput, TKey> requestKeySelector,
+            Func<TResponse, TKey> responseKeySelector
+            )
+        {
+            m_RequestFunc = requestFunc;
+            m_RequestFilter = requestFilter;
+            m_RequestKeySelector = (requestInput, requestOutput) => requestKeySelector(requestInput);
+            m_RequestKeySelectorWithRequestOutput = false;
             m_ResponseKeySelector = responseKeySelector;
         }
 
@@ -72,6 +103,24 @@ namespace Synchronizer
         /// <param name="millisecondsTimeout">超时时间</param>
         /// <returns>同步结果</returns>
         public SyncStatus SyncRequest(
+            TRequestInput requestInput,
+            out TRequestOutput requestOutput,
+            out TResponse response,
+            TimeSpan timeout
+            )
+        {
+            return m_RequestKeySelectorWithRequestOutput ? SyncRequestWithRequestOutput(requestInput, out requestOutput, out response, timeout) : SyncRequestWithoutRequestOutput(requestInput, out requestOutput, out response, timeout);
+        }
+
+        /// <summary>
+        /// 同步请求
+        /// </summary>
+        /// <param name="requestInput">请求参数</param>
+        /// <param name="requestOutput">请求结果</param>
+        /// <param name="response">异步结果</param>
+        /// <param name="millisecondsTimeout">超时时间</param>
+        /// <returns>同步结果</returns>
+        private SyncStatus SyncRequestWithRequestOutput(
             TRequestInput requestInput,
             out TRequestOutput requestOutput,
             out TResponse response,
@@ -113,10 +162,78 @@ namespace Synchronizer
         }
 
         /// <summary>
+        /// 同步请求
+        /// </summary>
+        /// <param name="requestInput">请求参数</param>
+        /// <param name="requestOutput">请求结果</param>
+        /// <param name="response">异步结果</param>
+        /// <param name="millisecondsTimeout">超时时间</param>
+        /// <returns>同步结果</returns>
+        private SyncStatus SyncRequestWithoutRequestOutput(
+            TRequestInput requestInput,
+            out TRequestOutput requestOutput,
+            out TResponse response,
+            TimeSpan timeout
+            )
+        {
+            SyncStatus syncStatus = SyncStatus.Failed;
+
+            response = default(TResponse);
+
+            requestOutput = default(TRequestOutput);
+            TRequestOutput requestOutputTemp = requestOutput;
+
+            TKey key = m_RequestKeySelector(requestInput, requestOutput);
+            SyncContext<TRequestInput, TRequestOutput, TResponse> syncContext = m_SyncContexts.GetOrAdd(key, k => new SyncContext<TRequestInput, TRequestOutput, TResponse>()
+            {
+                RequestInput = requestInput,
+                RequestOutput = requestOutputTemp,
+            });
+            syncContext.RequestInput = requestInput;
+            syncContext.RequestOutput = requestOutputTemp;
+
+            requestOutput = m_RequestFunc(requestInput);
+            syncContext.RequestOutput = requestOutputTemp;
+
+            if (m_RequestFilter(requestInput, requestOutput))
+            {
+                if (syncContext.ManualResetEvent.WaitOne(timeout))
+                {
+                    syncStatus = SyncStatus.Successful;
+                    response = syncContext.Response;
+                }
+                else
+                {
+                    syncStatus = SyncStatus.Timeout;
+                }
+            }
+
+            m_SyncContexts.TryRemove(key, out _);
+
+            return syncStatus;
+        }
+
+        /// <summary>
         /// 请求响应通知
         /// </summary>
         /// <param name="result"></param>
         public void FeedResponse(TResponse response)
+        {
+            if (m_RequestKeySelectorWithRequestOutput)
+            {
+                FeedResponseWithRequestOutput(response);
+            }
+            else
+            {
+                FeedResponseWithoutRequestOutput(response);
+            }
+        }
+
+        /// <summary>
+        /// 请求响应通知
+        /// </summary>
+        /// <param name="result"></param>
+        private void FeedResponseWithRequestOutput(TResponse response)
         {
             TKey key = m_ResponseKeySelector(response);
 
@@ -127,6 +244,20 @@ namespace Synchronizer
             syncContext.Response = response;
 
             syncContext.ManualResetEvent.Set();
+        }
+
+        /// <summary>
+        /// 请求响应通知
+        /// </summary>
+        /// <param name="result"></param>
+        private void FeedResponseWithoutRequestOutput(TResponse response)
+        {
+            TKey key = m_ResponseKeySelector(response);
+            if (m_SyncContexts.TryGetValue(key, out var syncContext))
+            {
+                syncContext.Response = response;
+                syncContext.ManualResetEvent.Set();
+            }
         }
     }
 }
--- UnitTest/RequestSynchronizerUnitTest.cs
+++ UnitTest/RequestSynchronizerUnitTest.cs
@@ -20,7 +20,7 @@ namespace UnitTest
         [DataRow("guid008", 4, 3, true, "guid008", "4", 4, SyncStatus.Timeout, null)]
         [DataRow("guid009", 4, 3, true, "guid009", "4", 5, SyncStatus.Timeout, null)]
         [DataRow("guid010", 4, 0, true, "guid010", "4", 1, SyncStatus.Timeout, null)]
-        public void SyncRequestTestMethod(
+        public void SyncRequestWithRequestOutputTestMethod(
             string requestInputSn,
             int requestInputArg,
             int syncTimeout,
@@ -45,6 +45,53 @@ namespace UnitTest
                 Thread.Sleep(TimeSpan.FromSeconds(responseDelay));
                 synchronizer.FeedResponse(new Response() { NS = responseSn, Value = responseValue });
             });
+
+            var actSyncStatus = synchronizer.SyncRequest(requestInput, out RequestOutput requestOutput, out Response response, TimeSpan.FromSeconds(syncTimeout));
+
+            Assert.IsNotNull(requestOutput);
+            Assert.IsTrue(requestOutputValue == requestOutput.Result);
+            Assert.IsTrue(actSyncStatus == syncStatus);
+            Assert.IsTrue(response?.Value == responseValueSynced);
+        }
+
+        [TestMethod]
+        [DataRow("guid001", -1, 3, false, "guid001", "-1", 1, SyncStatus.Failed, null)]
+        [DataRow("guid002", -4, 3, true, "guid002", "-4", 1, SyncStatus.Successful, "-4")]
+        [DataRow("guid003", 0, 3, true, "guid003", "0", 1, SyncStatus.Successful, "0")]
+        [DataRow("guid004", 1, 3, false, "guid004", "1", 1, SyncStatus.Failed, null)]
+        [DataRow("guid005", 4, 3, true, "guid005", "4", 1, SyncStatus.Successful, "4")]
+        [DataRow("guid006", 4, 3, true, "guid006", "4", 0, SyncStatus.Timeout, null)]
+        [DataRow("guid007", 4, 3, true, "guid007", "4", 2, SyncStatus.Successful, "4")]
+        [DataRow("guid008", 4, 3, true, "guid008", "4", 4, SyncStatus.Timeout, null)]
+        [DataRow("guid009", 4, 3, true, "guid009", "4", 5, SyncStatus.Timeout, null)]
+        [DataRow("guid010", 4, 0, true, "guid010", "4", 1, SyncStatus.Timeout, null)]
+        public void SyncRequestWithoutRequestOutputTestMethod(
+            string requestInputSn,
+            int requestInputArg,
+            int syncTimeout,
+            bool requestOutputValue,
+            string responseSn,
+            string responseValue,
+            int responseDelay,
+            SyncStatus syncStatus,
+            string responseValueSynced
+            )
+        {
+            RequestSynchronizer<RequestInput, RequestOutput, Response, string> synchronizer = new RequestSynchronizer<RequestInput, RequestOutput, Response, string>(
+                input => new RequestOutput() { Result = input.Arg % 2 == 0 },
+                (input, output) => output.Result,
+                input => input.SN,
+                response => response.NS);
+
+            RequestInput requestInput = new RequestInput() { SN = requestInputSn, Arg = requestInputArg };
+
+            Task.Run(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(responseDelay));
+                synchronizer.FeedResponse(new Response() { NS = responseSn, Value = responseValue });
+            });
+
+            Thread.Sleep(500);
 
             var actSyncStatus = synchronizer.SyncRequest(requestInput, out RequestOutput requestOutput, out Response response, TimeSpan.FromSeconds(syncTimeout));
 
